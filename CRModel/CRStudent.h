7ad3ad57fc9e8bc3a9c02b695261908614fe07c3
yslin2015 <359927530@qq.com>
2018-08-19 12:48:40 +0800

    add pod files

---
+++ CRModel/CRStudent.h
@@ -0,0 +1,12 @@
+//
+//  CRStudent.h
+//  
+//
+//  Created by yslin on 2018/8/19.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRStudent : NSObject
+
+@end
---
+++ CRModel/CRStudent.m
@@ -0,0 +1,12 @@
+//
+//  CRStudent.m
+//  
+//
+//  Created by yslin on 2018/8/19.
+//
+
+#import "CRStudent.h"
+
+@implementation CRStudent
+
+@end
